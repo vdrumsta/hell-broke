bd69a9f09179216c836f448919c5dad025cf4b01
Vilius Drumsta <vdrumsta@gmail.com>
2021-06-10 22:40:36 +0100

    Add tutorial reset button

--- Assets/Scripts/MapMenuRecordController.cs
+++ Assets/Scripts/MapMenuRecordController.cs
@@ -11,13 +11,29 @@ public class MapMenuRecordController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        DisplayMapRecords();
+    }
+
+    public void ResetTutorials()
+    {
+        for (int i = 0; i < _levelRecordTexts.Length; i++)
+        {
+            if (PlayerPrefs.HasKey("Level0" + (i + 1) + " Tutorial"))    // TODO: Make this work for levels greater than 9
+            {
+                PlayerPrefs.DeleteKey("Level0" + (i + 1) + " Tutorial");
+            }
+        }
+    }
+
+    private void DisplayMapRecords()
+    {
         for (int i = 0; i < _levelRecordTexts.Length; i++)
         {
             _levelRecordTexts[i].text = GetRecord(i + 1);
         }
     }
 
-    public string GetRecord(int level)
+    private string GetRecord(int level)
     {
         string record = "";
 
@@ -25,7 +41,6 @@ public class MapMenuRecordController : MonoBehaviour
         if (PlayerPrefs.HasKey("Level0" + level + " Highscore"))    // TODO: Make this work for levels greater than 9
         {
             float bestLevelTime = PlayerPrefs.GetFloat("Level0" + level + " Highscore");
-            Debug.Log("Level0" + level + " has time " + bestLevelTime);
             decimal d = (decimal)bestLevelTime;
             decimal truncated = decimal.Truncate(d * 100m) / 100m;
             record = truncated.ToString();
